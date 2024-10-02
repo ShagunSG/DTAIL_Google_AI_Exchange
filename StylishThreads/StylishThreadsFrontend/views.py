f1291e28214d73033db337484b7e2b5891d7ab87
ShagunSG <shaguneghatak@gmail.com>
2024-10-02 19:41:58 +0530

    backend establishing complete

--- StylishThreads/StylishThreadsFrontend/views.py
+++ StylishThreads/StylishThreadsFrontend/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, "home.html")
+    if request.method == "GET":
+        return render(request, "home.html")
+    if request.method == "POST":
+        user_id = request.POST.get('user_id')
+        name = request.POST.get('name')
