e0595c09e58a5bf2d6f4d58db4adb505fe3c613e
ShagunSG <shaguneghatak@gmail.com>
2024-10-02 23:22:30 +0530

    gave backend functionality

--- StylishThreads/StylishThreadsFrontend/views.py
+++ StylishThreads/StylishThreadsFrontend/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render, HttpResponse
+import pandas as pd
+from django.template.loader import render_to_string
 
 # Create your views here.
 def index(request):
     if request.method == "GET":
         return render(request, "home.html")
     if request.method == "POST":
+        print('post')
         user_id = request.POST.get('user_id')
         name = request.POST.get('name')
+        df = pd.read_csv('../static/generated_emails1.csv')
+        email = df[df['user_id'] == int(user_id)]['email'].values[0]
+        return render(request, "output.html", email=email)
