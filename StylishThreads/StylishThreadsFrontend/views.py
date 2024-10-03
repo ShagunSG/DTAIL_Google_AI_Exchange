26b0d71f7cb49191782698ea48807e1c33f3e781
ShagunSG <shaguneghatak@gmail.com>
2024-10-03 11:24:12 +0530

    made changes to views.py

--- StylishThreads/StylishThreadsFrontend/views.py
+++ StylishThreads/StylishThreadsFrontend/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render, HttpResponse
 import pandas as pd
 from django.template.loader import render_to_string
+from StylishThreads.static import generated_emails1
 
 # Create your views here.
 def index(request):
     if request.method == "GET":
-        return render(request, "home.html")
+        print('post')
+        user_id = request.POST.get('user_id')
+        name = request.POST.get('name')
+        df = pd.read_csv(generated_emails1)
+        email = df[df['user_id'] == int(user_id)]['email'].values[0]
+        return render(request, "output.html", email=email)
+        # return render(request, "home.html")
     if request.method == "POST":
         print('post')
         user_id = request.POST.get('user_id')
         name = request.POST.get('name')
-        df = pd.read_csv('../static/generated_emails1.csv')
+        df = pd.read_csv('StylishThreads/static/generated_emails1.csv')
         email = df[df['user_id'] == int(user_id)]['email'].values[0]
         return render(request, "output.html", email=email)
